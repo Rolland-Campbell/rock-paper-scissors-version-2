efba9b1e402aafba534b03b6e4a7e83959c7d02f
Bud <rollandcampbell@msn.com>
2019-08-14 18:41:26 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let playerPick = null;
+
+function computerChoose() {
+  let computerPick = Math.random();
+  if (computerPick < 0.34) {
+    return "rock";
+  } else if (computerPick <= 0.67) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function pickRock() {
+  playerPick = "rock";
+}
+
+let gameOutcome = document.querySelector("Who will WIN")
+
+function compare(playerPick, computerPick) {
+  if (playerPick == "rock") {
+    if (computerPick == "rock") {
+      return gameOutcome.textContent = "It is a TIE!"
+    }
+  }
+  if (playerPick == "paper") {
+    if (computerPick == "rock") {
+      return gameOutcome.textContent = "You LOSE!"
+    } else {
+      return gameOutcome.textContent = "You WIN!"
+    }
+  }
+  if (playerPick == "scissors") {
+    if (computerPick == "paper") {
+      return gameOutcome.textContent = "You WIN!"
+    } else {
+      return gameOutcome.textContent = "You LOSE!"
+    }
+  }
+}
